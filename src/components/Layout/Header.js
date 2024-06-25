3cba4ad07fc9fc7e0171d93fd5e8495d2ed86dad
bhanu prakash <myselfbhanuprakash7297@gmail.com>
2024-06-25 19:00:05 +0530

    updated logo and homepage

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -28,7 +28,9 @@ const Header = () => {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary ">
                 <div className="container-fluid">
-                    <Link to='/' className="navbar-brand" >Ecommerce App</Link>
+                    <Link to='/' className="navbar-brand" >
+                    <img src='/images/logo.jpg' alt='logo..' style={{width:'150px', height:'60px',marginLeft:'30px'}}/>
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
